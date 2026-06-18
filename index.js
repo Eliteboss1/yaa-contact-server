bf530baa9f21964c46856461bb8d2872e7bb5d46
Daniel Ipogah <105306985+Eliteboss1@users.noreply.github.com>
2026-06-18 11:10:53 +0100

    Rename email

--- index.js
+++ index.js
@@ -43,7 +43,7 @@ app.post("/send-mail", async (req, res) => {
       from: "House of YAA <onboarding@resend.dev>",
 
       // YOUR RECEIVING EMAIL
-      to: "admin@houseofyaa.com",
+      to: "Admin@houseofyaa.com",
 
       subject: `New House of YAA Inquiry from ${name}`,
 
