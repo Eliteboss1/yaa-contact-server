fbdd6d7336b1a631f9b5616a88d28a5bde944e9a
Daniel Ipogah <105306985+Eliteboss1@users.noreply.github.com>
2026-06-18 10:58:13 +0100

    New mail

--- index.js
+++ index.js
@@ -43,7 +43,7 @@ app.post("/send-mail", async (req, res) => {
       from: "House of YAA <onboarding@resend.dev>",
 
       // YOUR RECEIVING EMAIL
-      to: "darnielipogah@gmail.com",
+      to: "admin@houseofyaa.com",
 
       subject: `New House of YAA Inquiry from ${name}`,
 
